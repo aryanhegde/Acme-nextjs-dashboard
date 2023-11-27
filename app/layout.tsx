6813b618a5656032b94de2a23d22b23b4b44f3f3
unknown <aryanhegde>
2023-11-27 18:56:24 +0530

    second commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { Inter, Roboto_Mono } from 'next/font/google'
+ 
+import './ui/global.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+ 
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-mono',
+})
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={`${inter.variable} ${roboto_mono.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
